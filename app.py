359383802537e44db79780dc67487f261ef10f57
Dawnt <jmanuelhv9@gmail.com>
2022-07-09 22:47:29 -0500

    Dynamic Events
    Se ha implementado una forma de recuperar los eventos del servidor

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+from src.events import Events
+import os, dotenv
 
+dotenv.load_dotenv()
 app = Flask(__name__)
+threads = []
+events = Events(
+    guild_id="818251363012182023",
+    token=os.environ.get("DISCORD_TOKEN")
+) 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    elements = events.list
+    return render_template('index.html', events=elements[:3])
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
